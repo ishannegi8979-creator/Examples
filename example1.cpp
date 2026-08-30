6a31022561e68e7ea1b999d3abd78a6c21220088
ishannegi8979-creator <ishannegi8979@gmail.com>
2026-08-30 07:59:34 +0530

    .\example1.cpp

--- example1.cpp
+++ example1.cpp
@@ -7,14 +7,14 @@ int main () {
 
     cin >> n; 
 
-    for ( int row = 0 ; row < n ; row = row + 1)
+    for ( int l = 0 ; l < n ; l = l + 1)
     {
 
-        for ( int p = 0 ; p < n - row - 1 ; p = p + 1)
+        for ( int p = 0 ; p < n - l - 1 ; p = p + 1)
         {
             cout << " ";
         }
-        for( int j = 0 ; j < row + 1 ; j = j + 1)
+        for( int j = 0 ; j < l + 1 ; j = j + 1)
         {
 
             cout << "* " ;
